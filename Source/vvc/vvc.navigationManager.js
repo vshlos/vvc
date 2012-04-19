7e4fd2d422d09dfaee585a6fa0f3ec015d2a1ec5
= <=>
2012-04-19 09:51:00 -0700

    Created build script.

--- Source/vvc/vvc.navigationManager.js
+++ Source/vvc/vvc.navigationManager.js
@@ -1,18 +1,29 @@
-﻿/*
-    Page manager class
-*/
+﻿
 
 /*global jQuery, $, document, console, vvc */
 "use strict";
 vvc.createClass("vvc", "defaultNavigationManager", null, {
+    /// <summary>
+    /// Default class for handling registering and navigating to routes based on url changes and hash url changes.
+    /// <summary>
 
 
     init: function () {
+        /// <summary>
+        /// Initializes the DefaultNavigationManager by defining the private variables.
+        /// <summary>
+
         this.routes = {};
     },
     registerRoutes: function (paths) {
-        var handler;
+        /// <summary>
+        /// Registers a collection of routes
+        /// <summary>
+        /// <param name="paths" type="Object">The object which maps String path names to handler functions.<param>
+        /// <returns></returns>
 
+
+        var handler;
 
 
         for (var path in paths) {
@@ -22,6 +33,14 @@ vvc.createClass("vvc", "defaultNavigationManager", null, {
         }
     },
     registerRoute: function (path, handler) {
+        /// <summary>
+        /// Registers a single route by path and handler function
+        /// <summary>
+        /// <param name="path" type="String">The path to register and look for in navigation events.<param>
+        /// <param name="handler" type="Function">The function which will be called when the path is identified.<param>
+
+
+
         var parts = path, root = this.routes, part = null;
 
         if (typeof path === 'string') {
@@ -41,11 +60,16 @@ vvc.createClass("vvc", "defaultNavigationManager", null, {
 
         root[parts[parts.length - 1]] = handler;
 
-
-        return root;
     },
-
     showPage: function (path) {
+        /// <summary>
+        /// Calls the handler by path name.
+        /// <summary>
+        /// <param name="path" type="String">The path to find and call a handler for.<param>
+        /// <returns>Returns true if the handler was found and called, otherwise false.</returns>
+        
+
+
         var parts = path.split("/").reverse();
         var root = this.routes;
 
@@ -53,31 +77,51 @@ vvc.createClass("vvc", "defaultNavigationManager", null, {
             root = root[parts.pop()];
         }
 
-        if (root)
+        //the handler was found, call it.
+        if (root){
             root(path);
+            return true;
+        }
 
+        return false;
     },
     start: function () {
+        /// <summary>
+        /// Starts handling calls of hash changes checks the current hash for a path.
+        /// <summary>
+        
 
         var that = this;
         if (typeof window.onhashchange !== 'undefined') {
             window.onhashchange = function () {
-                that.hashChanged();
+                that._hashChanged();
             };
         }
 
-        that.hashChanged();
+        that._hashChanged();
 
 
     },
-    hashChanged: function () {
+    _hashChanged: function () {
+        /// <summary>
+        /// Identifies the curreht path based on hash value and calls that handler.
+        /// <summary>
+        /// <returns>Returns true if the handler was found and called, otherwise false.</returns>
+        
+        
         var pathStart = window.location.href.indexOf("#"),
             path = pathStart > 0 ? window.location.href.substr(pathStart + 1) : "/"
 
-        this.showPage(path);
+        return this.showPage(path);
     },
 
     redirect: function () {
+        /// <summary>
+        /// Takes the arguments and converts them to a url. Next this path is navigated to by the browser.
+        /// <summary>
+        /// <param name="arguments">The parts of the url to navigate to.</param>
+        
+
         var path = Array.prototype.slice.call(arguments).join("/"),
             currPathStart = window.location.href.indexOf("#"),
             base = currPathStart > 0 ? window.location.href.substr(0, currPathStart) : window.location.href;
@@ -92,6 +136,8 @@ vvc.createClass("vvc", "defaultNavigationManager", null, {
 
 
 
-
+/// <summary>
+/// The singleton instance of the default navigation manager. Also a place for overriding the default with a custom one.
+/// <summary>
 vvc.navManager = new vvc.defaultNavigationManager();
 
